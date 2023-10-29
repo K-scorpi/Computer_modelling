62200023c81868b24ba69521e086bdb1122bb871
K-scorpi <98895710+K-scorpi@users.noreply.github.com>
2023-10-29 12:20:37 +0500

    Update task_6.py

--- task3_new/task_6.py
+++ task3_new/task_6.py
@@ -1,7 +1,14 @@
 from datetime import*
-d=input()
-date1=date.today()
-date2=datetime.strptime(d, '%d.%m.%Y')
-d1=date(date1.year, date1.month, date1.day)
-d2=date(date2.year, date2.month, date2.day)
-print(-((d2-d1).days))
+
+
+d = input()
+
+
+def f(d):
+    date1=date.today()
+    date2=datetime.strptime(d, '%d.%m.%Y')
+    d1=date(date1.year, date1.month, date1.day)
+    d2=date(date2.year, date2.month, date2.day)
+    print(-((d2-d1).days))
+
+f(d)
