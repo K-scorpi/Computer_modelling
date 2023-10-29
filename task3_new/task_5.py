40d4a75868caabb02d45175fbd579e6f7bc93926
K-scorpi <98895710+K-scorpi@users.noreply.github.com>
2023-10-29 12:19:50 +0500

    Update task_5.py

--- task3_new/task_5.py
+++ task3_new/task_5.py
@@ -1,9 +1,12 @@
-a=int(input("Введите a"))
-b=int(input("Введите b"))
-c=int(input("Введите c"))
-x=int(input("Введите x"))
-y=int(input("Введите y"))
-if (x>=a and y>=b) or (x>=c and y>=b):
-    print("кирпич прошел")
-else:
-    print ("не прошел")
+def d(a : int,
+      b : int,
+      c : int,
+      x : int,
+      y: int=1):
+    if (x >= a and y >= b) or (x >= c and y >= b):
+        print("кирпич прошел")
+    else:
+        print("не прошел")
+
+
+d(input(), input(), input(), input(), input())
